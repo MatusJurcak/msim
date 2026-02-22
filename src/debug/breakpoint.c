24db3ad9cf5f5831978f72e93013cd9e71a44552
Matus Jurcak <jurcak.matus@gmail.com>
2026-02-22 18:58:06 +0100

    SH-2E Fix DAP in tests config

--- src/debug/breakpoint.c
+++ src/debug/breakpoint.c
@@ -309,7 +309,6 @@ breakpoint_t *breakpoint_find_by_address(list_t breakpoints,
  */
 bool breakpoint_check_for_code_breakpoints(void)
 {
-    // TODO: add RV support
     // TODO: add SH2E support
     bool hit = false;
     device_t *dev = NULL;
